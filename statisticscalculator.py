222cf6883402820f284ee625b9691485869b6dfb
MohammadReza Omrani <52138433+mohammad-reza-omrani@users.noreply.github.com>
2025-01-16 07:54:37 -0600

    Add files via upload

---
+++ statisticscalculator.py
@@ -0,0 +1,7 @@
+import statistics
+
+data = [2, 4, 6, 8, 10]
+print("Mean:", statistics.mean(data))
+print("Median:", statistics.median(data))
+print("Mode:", statistics.mode(data))
+print("Standard Deviation:", statistics.stdev(data))
