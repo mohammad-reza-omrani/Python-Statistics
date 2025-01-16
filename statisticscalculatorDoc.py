9182b7c9a03f5cbe042d4dc41caeb950718b9120
MohammadReza Omrani <52138433+mohammad-reza-omrani@users.noreply.github.com>
2025-01-16 08:43:59 -0600

    Add files via upload

---
+++ statisticscalculatorDoc.py
@@ -0,0 +1,14 @@
+# ========================================================================================== Import Section
+# imports the statistics module,for statistical measures like mean, median, mode, and standard deviation
+
+# ============================================================================================ Data Section
+# defines a list of numbers called data
+
+# =========================================================================================== Print Section
+# statistics.mean(data) calculates the mean (average) of the data list, then printed out along with the label "Mean:"
+
+# statistics.median(data) calculates the median (middle value) of the data list, then printed out along with the label "Median:"
+
+# statistics.mode(data) calculates the mode (most frequently) of the data list, then printed out along with the label "Mode:"
+
+# statistics.stdev(data) calculates the standard deviation (variation or dispersion amount) of the data list, then printed out along with the label "Standard Deviation:"
